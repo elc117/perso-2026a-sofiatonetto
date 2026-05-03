1c6e049e15d461592e19869817ca864e097b37ce
sofiatonetto <103641763+sofiatonetto@users.noreply.github.com>
2026-05-03 13:24:57 +0000

    buscas

--- cadastro.hs
+++ cadastro.hs
@@ -23,8 +23,7 @@ matriculaCadastrada Bolsistas matricula =
     lenght (filter(\b -> matricula b == matricula) Bolsistas) > 0
 
 cadastrar :: [Bolsistas] -> String -> String -> String -> String -> String -> [Bolsistas]
-cadastrar bs matricula nome email linha_pesquisa horarios_livres
-    Bolsistas ++ [Bolsistas matricula nome email linha_pesquisa horarios_livres]
+cadastrar bs matricula nome email linha_pesquisa horarios_livres = Bolsistas ++ [Bolsistas matricula nome email linha_pesquisa horarios_livres]
     where
         jaCadastrado = matriculaCadastrada Bolsistas matricula
 
@@ -35,3 +34,9 @@ editarBolsista Bolsistas matricula horarios_livres_editado = map $ atualizaBolsi
 
 listarBolsistas :: [Bolsistas] -> [String]
 listarBolsistas Bolsistas = map (\b matricula b ++ " . " ++ nome b ++ " . " ++ email b ++ " . " ++ horarios_livres ++ " . " ++ linha_pesquisa b) Bolsistas
+
+buscarPorPesquisa :: [Bolsistas] -> String -> [Bolsistas]
+buscarPorPesquisa Bolsistas linha_pesquisa = filter(\b -> linha_pesquisa b == linha_pesquisa) Bolsistas
+
+buscaPorMatricula :: [Bolsistas] -> String -> [Bolsistas]
+buscaPorMatricula Bolsistas matricula = filter(\b -> matricula b == matricula) Bolsistas
